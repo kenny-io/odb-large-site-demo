fd8108c0153f95849ebd756d79a0281e80172514
kenny-io <ekeneeze3@gmail.com>
2021-05-24 11:38:34 +0100

    odb large site

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <Testimonials />
+    <Features />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header";
+import Testimonials from "../components/Testimonials";
+import Features from "../components/Features";
+
+export default {
+  components: {
+    Header,
+    Testimonials,
+    Features
+  }
+};
+</script>
