4cd98f13853e35bf914ebf6dc7cf51ab380139ab
kenny-io <ekeneeze3@gmail.com>
2021-07-07 18:25:55 +0400

    perfectly working site

--- components/Nav.vue
+++ components/Nav.vue
@@ -18,26 +18,26 @@
         <nav class="hidden md:flex space-x-10">
           <a
             href="/shop"
-            class="text-base font-medium text-gray-500 hover:text-gray-100"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Shop
           </a>
           <a
             href="/about"
-            class="text-base font-medium text-gray-500 hover:text-gray-100"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
           >
             About
           </a>
           <a
             href="/docs"
-            class="text-base font-medium text-gray-500 hover:text-gray-100"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Docs
           </a>
 
           <a
             href="/blog"
-            class="text-base font-medium text-gray-500 hover:text-gray-100"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Blog
           </a>
@@ -45,13 +45,13 @@
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <a
             href="#"
-            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-100"
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Sign in
           </a>
           <a
             href="#"
-            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-900 hover:bg-red-100"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-900 hover:bg-red-900"
           >
             Sign up
           </a>
